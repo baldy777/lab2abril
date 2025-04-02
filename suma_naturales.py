daff10a11c5c3b2247a37664c0c1c5da1c95d156
baldy777 <jhoelquispetapia315@gmail.com>
2025-04-02 09:52:24 -0400

    laboratorio 2 de abril

---
+++ suma_naturales.py
@@ -0,0 +1,23 @@
+import timeit
+n = 100
+
+def suma_iterativa(n):
+    suma = 0
+    for i in range(1, n+1):
+        suma += i
+    return suma
+
+def suma_recursiva(n):
+    if n == 0:
+        return 0
+    return n + suma_recursiva(n - 1)
+
+def suma_formula(n):
+    return n * (n + 1) // 2
+
+
+
+print("tiempo iterativo: ", timeit.timeit(lambda: suma_iterativa(n)), " segundos")
+print("tiempo recursivo: ", timeit.timeit(lambda: suma_recursiva(n)), " segundos")
+print("tiempo formula: ", timeit.timeit(lambda: suma_formula(n)), " segundos")
+    
