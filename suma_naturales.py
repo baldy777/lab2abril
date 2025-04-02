a2e7fc61d0d874f3b64a808871052d71e20e0c41
baldy777 <jhoelquispetapia315@gmail.com>
2025-04-02 09:58:02 -0400

    laboratorio 2 abril 2P

--- suma_naturales.py
+++ suma_naturales.py
@@ -15,7 +15,9 @@ def suma_recursiva(n):
 def suma_formula(n):
     return n * (n + 1) // 2
 
+assert suma_iterativa(n) == suma_recursiva(n) == suma_formula(n), "¡Las funciones no coinciden!"
 
+print("✅ Todas las funciones dan el mismo resultado.")
 
 print("tiempo iterativo: ", timeit.timeit(lambda: suma_iterativa(n)), " segundos")
 print("tiempo recursivo: ", timeit.timeit(lambda: suma_recursiva(n)), " segundos")
